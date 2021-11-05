037331cb4935fcb1f055ac9b3691a085a2b09749
FranGuerre <franguerrero054@gmail.com>
2021-11-04 23:34:02 -0300

    dos desafios nuevos

--- script.js
+++ script.js
@@ -17,10 +17,13 @@ function selector() {
             desafio2();
             break;
           case 3:
-             desafio3();
+            desafio3();
             break;
           case 4:
-
+            simuladorInteractivo();
+            break;
+          case 5:
+            funcionesRelacionadas();
             break;
         }
       }
@@ -38,7 +41,7 @@ function desafio1() {
 
 function desafio2() {
   let entradaNum = Number(prompt("Ingresá un número."));
-  if(entradaNum % 2 === 0) {
+  if (entradaNum % 2 === 0) {
     alert("El número que ingresaste es par.");
   } else {
     alert("El número que ingresaste es impar.");
@@ -47,20 +50,103 @@ function desafio2() {
 
 function desafio3() {
   let n, m, r;
-  
+
   do {
     n = parseInt(prompt("Ingresa un nro entre 1 y 10 para ver sus multiplos"));
-    
-    if(n < 2) {
-       alert("El numero que ingresaste es menor que 2");
-    } else if(n > 9) {
+
+    if (n < 2) {
+      alert("El numero que ingresaste es menor que 2");
+    } else if (n > 9) {
       alert("El numero que ingresaste es mayor que 9");
     }
-  } while(n < 2 || n > 9); 
-  
-  for(let i = 0; i < 10; i++) {
+  } while (n < 2 || n > 9);
+
+  for (let i = 0; i < 10; i++) {
     m = i + 1;
     r = n * m;
     alert(`${m} x ${n} = ${r}`);
   }
 }
+
+//  Simulador Interactivo
+
+
+function simuladorInteractivo() {
+  let valorCuenta, propina, valorFinal;
+
+  do {
+    valorCuenta = parseInt(prompt("**CALCULADORA DE PROPINA**\rIngresa el valor de la cuenta"));
+  } while (isNaN(valorCuenta));
+
+  do {
+    propina = parseInt(prompt("**CALCULADORA DE PROPINA**\rIngresa la opción que desees\r1. 10%\r2. 15%\r3. 20%"));
+  } while (isNaN(propina) || propina < 1 || propina > 3);
+
+  valorFinal = calcularPropina(valorCuenta, propina);
+
+  alert(`Deberás dejar \$${valorFinal} de propina.`);
+
+}
+
+function calcularPropina(cuenta, codPropina) {
+  let propina;
+  switch (codPropina) {
+    case 1:
+      propina = 0.1;
+      break;
+    case 2:
+      propina = 0.15;
+      break;
+    case 3:
+      propina = 0.2;
+      break;
+    case 4:
+      alert("Error calculando");
+      return 0;
+  }
+  return cuenta * propina;
+}
+
+
+//  Fin Simulador Interactivo
+
+
+// Funciones Relacionadas
+
+
+function funcionesRelacionadas() {
+  let nAnimales;
+
+  nAnimales = cuantosAnimales();
+
+  mostrarAnimales(nAnimales);
+}
+
+function cuantosAnimales() {
+  let n;
+  do {
+    n = parseInt(prompt("¿Cuántos animales deseas ver?(Máximo tres)"));
+  } while (isNaN(n) || n < 1 || n > 3);
+
+
+  return n;
+}
+
+function mostrarAnimales(n) {
+
+  for(let i = 0; i < n; i++) {
+    mostrarAnimal(i);
+  }
+}
+
+function mostrarAnimal(animal) {
+  const animales = ["▕▔▔╲╱▔▔▔╲╱▔▔▏\r┈╲＿╱╰╮┈╭╯╲＿╱\r┈┈┈▏▉╮┈╭▉▕\r┈┈╱╲╰╰┊╯╯╱╲\r┈╱╰▕╰╰┳╯╯▏╯╲\r▕╰╰╰╲╰┻╯╱╯╯╯▏\r▕╰╰╰╰▔▔▔╯╯╯╯▏\r▕╰╰╰╰╰╮╭╯╯╯╯▏\r┈╲╭╮┈╰╮╭╯╭╮╱\r┈┈┫┣╭━━━╮┫┃\r┈┈┃┃┃┈┈┈┃┃┃\r┈┈┗┛┛┈┈┈┗┗┛", "╱▏┈┈┈┈┈┈▕╲▕╲┈┈┈\r▏▏┈┈┈┈┈┈▕▏▔▔╲┈┈\r▏╲┈┈┈┈┈┈╱┈▔┈▔╲┈\r╲▏▔▔▔▔▔▔╯╯╰┳━━▀\r┈▏╯╯╯╯╯╯╯╯╱┃┈┈┈\r┈┃┏┳┳━━━┫┣┳┃┈┈┈\r┈┃┃┃┃┈┈┈┃┃┃┃┈┈┈\r┈┗┛┗┛┈┈┈┗┛┗┛┈┈┈", "┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\r┈╭━━━━━╮┈┈┈┈┈┈┈┈\r┈╰┳━━━━╰╮╭━╮┈┈┈┈\r┈┈╰┳━━━━┣╯▊╲┈┈┈┈\r┈┈┈╰━┳━━╯╰╱▔┈┈┈┈\r┈┈┈┈┈╰━┳┳╯┈┈┈┈┈┈\r━━━━━━━┻┻━━━━━━━"];
+  alert(animales[animal]);
+}
+
+
+// fin Funciones Relacionadas
+
+
+
+// FUNCIONES
